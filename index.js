cf7a1c6e38847ecd1837543093e876d0a7ac2a76
DenysHV1 <denyagarkusha2@gmail.com>
2024-10-27 18:29:14 +0000

    Deploying to gh-pages from @ DenysHV1/DENYS_HV-PORTFOLIO@3b9d47f2fffabc485001b8bce313563b871ef7bb 🚀

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import{h,p as w}from"./assets/header-CALUihft.js";import{S as g}from"./assets/vendor-BiMJRTlQ.js";function m(s,a){function l(i){const t=[];for(let e=i.length-1;e>=0;e--)t.push(i[e]);const o=t.length;return t.map(({name:e,id:r,status:n,imgPreview:c,codeLink:p,siteLink:d,technology1:u,technology2:f})=>{if(n&&r>o-10)return`
-			<li class="swiper-slide" id="${r}">
+import{h,p as w}from"./assets/header-CALUihft.js";import{S as g}from"./assets/vendor-BiMJRTlQ.js";function m(t,i){function l(s){const r=[];for(let e=s.length-1;e>=0;e--)r.push(s[e]);const o=r.length;return r.map(({name:e,id:a,status:n,imgPreview:c,codeLink:p,siteLink:d,technology1:u,technology2:f})=>{if(n&&a>o-10)return`
+			<li class="swiper-slide" id="${a}">
       <h3 class="name-info-git">${e}</h3>
 		<img
 		class="gallery-image"
@@ -19,5 +19,5 @@ import{h,p as w}from"./assets/header-CALUihft.js";import{S as g}from"./assets/ve
     </div>
 	</div>
 
-	</li>`}).join("")}a.insertAdjacentHTML("beforeend",l(s))}function b(){return new g(".swiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{type:"bullets",el:".swiper-pagination",clickable:!0,dynamicBullets:!0},simulateTouch:!0,touchRatio:1,touchAngle:45,grabCursor:!0,hashNavigation:{watchState:!0},keyboard:{enabled:!0,onlyInViewport:!0,pageUpDown:!0},autoHeight:!1,slidesPerView:1,watchOverflow:!0,spaceBetween:20,slidesPerGroup:1,centeredSlides:!0,initialSlide:0,loop:!0,freeMode:!1,autoplay:{delay:5e3,stopOnLastSlide:!1,disableOnInteraction:!1},speed:800,direction:"horizontal",effect:"coverflow",coverflowEffect:{rotate:0,stretch:80,slideShadows:!0},breakpoints:{320:{slidesPerView:1},768:{slidesPerView:2},1280:{slidesPerView:3}}})}h();const v=document.querySelector(".swiper-wrapper");m(w,v);b();
+	</li>`}).join("")}i.insertAdjacentHTML("beforeend",l(t))}function v(){return new g(".swiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{type:"bullets",el:".swiper-pagination",clickable:!0,dynamicBullets:!0},simulateTouch:!0,touchRatio:1,touchAngle:45,grabCursor:!0,hashNavigation:{watchState:!0},keyboard:{enabled:!0,onlyInViewport:!0,pageUpDown:!0},autoHeight:!1,slidesPerView:1,watchOverflow:!0,spaceBetween:20,slidesPerGroup:1,centeredSlides:!0,initialSlide:0,loop:!0,freeMode:!1,autoplay:{delay:5e3,stopOnLastSlide:!1,disableOnInteraction:!1},speed:800,direction:"horizontal",effect:"coverflow",coverflowEffect:{rotate:0,stretch:80,slideShadows:!0},breakpoints:{320:{slidesPerView:1},768:{slidesPerView:2},1280:{slidesPerView:3}}})}h();const b=document.querySelector(".swiper-wrapper");m(w,b);v();document.querySelectorAll('a[target="_blank"]').forEach(t=>{t.addEventListener("click",function(i){window.open(this.href),i.preventDefault()})});
 //# sourceMappingURL=index.js.map
