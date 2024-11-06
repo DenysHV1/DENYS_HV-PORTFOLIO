6b8787214e78dc262820cf4fb0ef0debbf1b4f70
DenysHV1 <denyagarkusha2@gmail.com>
2024-11-06 22:10:54 +0000

    Deploying to gh-pages from @ DenysHV1/DENYS_HV-PORTFOLIO@5915efbbf365ddad4acaaf6b9c49986d68df9df3 🚀

--- web-apps.js
+++ web-apps.js
@@ -1,2 +1,2 @@
-import{h as a}from"./assets/header-BcrsyD0G.js";import{p as s}from"./assets/project-arr-nlKO3b8n.js";import{r as p,o as i,p as o,a as t,l as n}from"./assets/openLibraries-D_dGUL92.js";a();const{webAppsEl:r,webAppsKey:b,webAppsPagination:l}=t;r.insertAdjacentHTML("beforeend",p(s,b,n));const e=document.querySelectorAll(".web-appsLibraries");i(e);o(r,l,e);
+import{h as a}from"./assets/header-DeGCEMt5.js";import{p as s}from"./assets/project-arr-nlKO3b8n.js";import{r as p,o as i,p as o,a as t,l as n}from"./assets/openLibraries-D_dGUL92.js";a();const{webAppsEl:r,webAppsKey:b,webAppsPagination:l}=t;r.insertAdjacentHTML("beforeend",p(s,b,n));const e=document.querySelectorAll(".web-appsLibraries");i(e);o(r,l,e);
 //# sourceMappingURL=web-apps.js.map
--- websites.js
+++ websites.js
@@ -1,2 +1,2 @@
-import{h as s}from"./assets/header-BcrsyD0G.js";import{p as i}from"./assets/project-arr-nlKO3b8n.js";import{r as a,o as t,p as o,a as n,l as b}from"./assets/openLibraries-D_dGUL92.js";s();const{websitesEl:e,websitesKey:p,websitesPagination:l}=n;e.insertAdjacentHTML("beforeend",a(i,p,b));const r=document.querySelectorAll(".websitesLibraries");t(r);o(e,l,r);
+import{h as s}from"./assets/header-DeGCEMt5.js";import{p as i}from"./assets/project-arr-nlKO3b8n.js";import{r as a,o as t,p as o,a as n,l as b}from"./assets/openLibraries-D_dGUL92.js";s();const{websitesEl:e,websitesKey:p,websitesPagination:l}=n;e.insertAdjacentHTML("beforeend",a(i,p,b));const r=document.querySelectorAll(".websitesLibraries");t(r);o(e,l,r);
 //# sourceMappingURL=websites.js.map
