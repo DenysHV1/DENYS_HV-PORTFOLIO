3e071dc06c5b83f472e2823d30b106a3af1de017
DenysHV1 <denyagarkusha2@gmail.com>
2024-11-17 10:52:44 +0000

    Deploying to gh-pages from @ DenysHV1/DENYS_HV-PORTFOLIO@9dca070ff1fefd308985abd2a827e0243429e4a9 🚀

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import{h}from"./assets/header-B4c5GArV.js";import{p as w}from"./assets/project-arr-DPN6WERs.js";import{S as m}from"./assets/vendor-BiMJRTlQ.js";function g(t,i){function o(s){const r=[];for(let e=s.length-1;e>=0;e--)r.push(s[e]);const l=r.length;return r.map(({name:e,id:a,status:n,imgPreview:c,codeLink:p,siteLink:d,technology1:u,technology2:f})=>{if(n&&a>l-10)return`
+import{h as w}from"./assets/header-B4c5GArV.js";import{p as m}from"./assets/project-arr-DKbqMFmA.js";import{S as g}from"./assets/vendor-BiMJRTlQ.js";function b(t,i){function o(s){const r=[];for(let e=s.length-1;e>=0;e--)r.push(s[e]);const l=r.length;return r.map(({name:e,id:a,status:n,imgPreview:c,codeLink:p,siteLink:d,technology1:u,technology2:f,technology3:h})=>{if(n&&a>l-10)return`
 			<li class="swiper-slide" id="${a}">
       <h3 class="name-info-git">${e}</h3>
 		<img
@@ -12,6 +12,7 @@ import{h}from"./assets/header-B4c5GArV.js";import{p as w}from"./assets/project-a
     <div class="project-item-info-left">
         <p class="technology">${u}</p>
         <p class="technology">${f}</p>
+        <p class="technology">${h}</p>
     </div>
     <div class="project-item-info-right">
       <a href="${d}" class="last-project-item-btn">Site</a>
@@ -19,5 +20,5 @@ import{h}from"./assets/header-B4c5GArV.js";import{p as w}from"./assets/project-a
     </div>
 	</div>
 
-	</li>`}).join("")}i.insertAdjacentHTML("beforeend",o(t))}function b(){return new m(".swiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{type:"bullets",el:".swiper-pagination",clickable:!0,dynamicBullets:!0},simulateTouch:!0,touchRatio:1,touchAngle:45,grabCursor:!0,hashNavigation:{watchState:!0},keyboard:{enabled:!0,onlyInViewport:!0,pageUpDown:!0},autoHeight:!1,slidesPerView:1,watchOverflow:!0,spaceBetween:20,slidesPerGroup:1,centeredSlides:!0,initialSlide:0,loop:!0,freeMode:!1,autoplay:{delay:5e3,stopOnLastSlide:!1,disableOnInteraction:!1},speed:400,direction:"horizontal",effect:"coverflow",coverflowEffect:{rotate:0,stretch:80,slideShadows:!0},breakpoints:{320:{slidesPerView:1},768:{slidesPerView:2},1280:{slidesPerView:3}}})}h();const v=document.querySelector(".swiper-wrapper");g(w,v);b();document.querySelectorAll('a[target="_blank"]').forEach(t=>{t.addEventListener("click",function(i){i.preventDefault(),setTimeout(()=>{window.open(this.href,"_blank")},100)})});
+	</li>`}).join("")}i.insertAdjacentHTML("beforeend",o(t))}function v(){return new g(".swiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{type:"bullets",el:".swiper-pagination",clickable:!0,dynamicBullets:!0},simulateTouch:!0,touchRatio:1,touchAngle:45,grabCursor:!0,hashNavigation:{watchState:!0},keyboard:{enabled:!0,onlyInViewport:!0,pageUpDown:!0},autoHeight:!1,slidesPerView:1,watchOverflow:!0,spaceBetween:20,slidesPerGroup:1,centeredSlides:!0,initialSlide:0,loop:!0,freeMode:!1,autoplay:{delay:5e3,stopOnLastSlide:!1,disableOnInteraction:!1},speed:400,direction:"horizontal",effect:"coverflow",coverflowEffect:{rotate:0,stretch:80,slideShadows:!0},breakpoints:{320:{slidesPerView:1},768:{slidesPerView:2},1280:{slidesPerView:3}}})}w();const S=document.querySelector(".swiper-wrapper");b(m,S);v();document.querySelectorAll('a[target="_blank"]').forEach(t=>{t.addEventListener("click",function(i){i.preventDefault(),setTimeout(()=>{window.open(this.href,"_blank")},100)})});
 //# sourceMappingURL=index.js.map
