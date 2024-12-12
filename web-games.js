a7cbd49e7d328d9f5f28e5ae0a371341e713a361
DenysHV1 <denyagarkusha2@gmail.com>
2024-12-12 17:25:42 +0000

    Deploying to gh-pages from @ DenysHV1/DENYS_HV-PORTFOLIO@0fc74da56bfa53fec1b5f35d9e480c5242cfb977 🚀

--- web-games.js
+++ web-games.js
@@ -1,2 +1,2 @@
-import{h as a}from"./assets/header-B4c5GArV.js";import{p as s}from"./assets/project-arr-BR_oZcxM.js";import{r as i,o,p as t,a as n,l as m}from"./assets/openLibraries-uSaQstZP.js";a();const{webGamesEl:e,webGamesKey:b,webGamesPagination:p}=n;e.insertAdjacentHTML("beforeend",i(s,b,m));const r=document.querySelectorAll(".web-gamesLibraries");o(r);t(e,p,r);
+import{h as a}from"./assets/header-B4c5GArV.js";import{p as s}from"./assets/project-arr-BY_gKk3u.js";import{r as i,o,p as t,a as n,l as m}from"./assets/openLibraries-uSaQstZP.js";a();const{webGamesEl:e,webGamesKey:b,webGamesPagination:p}=n;e.insertAdjacentHTML("beforeend",i(s,b,m));const r=document.querySelectorAll(".web-gamesLibraries");o(r);t(e,p,r);
 //# sourceMappingURL=web-games.js.map
