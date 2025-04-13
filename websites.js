74257c991041211a7c42442ba5595a458c7b0bfa
DenysHV1 <denyagarkusha2@gmail.com>
2025-04-13 22:34:00 +0000

    Deploying to gh-pages from @ DenysHV1/DENYS_HV-PORTFOLIO@4f90d7c6bb00de01713cfe516fbcc343cd432cac 🚀

--- websites.js
+++ websites.js
@@ -1,2 +1,2 @@
-import{s,h as i}from"./assets/header-C2Kr-khp.js";import{p as a}from"./assets/project-arr-CFThWp5X.js";import{r as t,o,p as n,a as p,l as b}from"./assets/openLibraries-uSaQstZP.js";s();i();const{websitesEl:e,websitesKey:l,websitesPagination:c}=p;e.insertAdjacentHTML("beforeend",t(a,l,b));const r=document.querySelectorAll(".websitesLibraries");o(r);n(e,c,r);
+import{s,h as i}from"./assets/header-NVPK-VQA.js";import{p as a}from"./assets/project-arr-CFThWp5X.js";import{r as t,o,p as n,a as p,l as b}from"./assets/openLibraries-uSaQstZP.js";s();i();const{websitesEl:e,websitesKey:l,websitesPagination:c}=p;e.insertAdjacentHTML("beforeend",t(a,l,b));const r=document.querySelectorAll(".websitesLibraries");o(r);n(e,c,r);
 //# sourceMappingURL=websites.js.map
